74dec2b754058c13d27b370dfab01a2e9ff4dbcd
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-05-15 23:18:48 -0400

    made fish config cross platform

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,8 @@ bass source ~/.bashrc
 
 set fish_greeting 
 
-alias open "nautilus"
 alias docker "sudo docker"
-alias config "/usr/bin/git --git-dir=/home/lester/.cfg/ --work-tree=/home/lester"
+alias config "/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 
 fish_vi_key_bindings
 
