3f95941036ea092997ecf56eddb3b4fa15ea6f76
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-06-12 01:59:02 -0400

    feat: fzf for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,5 +5,8 @@ set fish_greeting
 alias docker "sudo docker"
 alias config "/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 
+set -g FZF_CTRL_T_COMMAND "find \$dir -not \( -path '*/node_modules' -prune \) \
+  -not \( -path '*/.git' -prune \)"
+
 fish_vi_key_bindings
 
