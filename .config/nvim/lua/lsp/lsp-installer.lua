c307134f6490aee666dd086fbd9c3d7384a21118
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-07-12 21:50:04 -0400

    fix css and html settings for lsp

--- .config/nvim/lua/lsp/lsp-installer.lua
+++ .config/nvim/lua/lsp/lsp-installer.lua
@@ -27,10 +27,7 @@ require("nvim-lsp-installer").setup({
   }
 })
 
-local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status_ok then
-  return
-end
+local lspconfig = require "lspconfig"
 
 for _, server in pairs(servers) do
   local opts = {
