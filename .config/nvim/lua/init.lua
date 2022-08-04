525845b95891db077aaea5ed0ebc28f4b53e8f75
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-08-04 15:46:54 -0400

    fix: recursive autocmd 😬

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,3 +1,10 @@
+local formatGroup = vim.api.nvim_create_augroup("Format", { clear = true });
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  command = "lua vim.lsp.buf.formatting_seq_sync()",
+  group = formatGroup
+})
+
 require "lsp.init"
 require "me.treesitter"
 require "me.nvim-cmp"
--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -3,14 +3,6 @@ if not status_ok then
   return
 end
 
-
-local formatGroup = vim.api.nvim_create_augroup("Format", { clear = true });
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  command = "autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync()",
-  group = formatGroup
-})
-
 require "lsp.mason"
 require "lsp.handlers".setup()
 require "lsp.null-ls"
