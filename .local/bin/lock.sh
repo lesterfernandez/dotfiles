e60859ff160e9ea9e57476762be51581617a7c6a
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-08-19 02:10:43 -0400

    i3

---
+++ .local/bin/lock.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+betterlockscreen -l blur
+# BLANK='#00000000'
+# CLEAR='#ffffff22'
+# DEFAULT='#ebdbb2'
+# TEXT='#fbf1c7'
+# WRONG='#83a598'
+# VERIFYING='#83a598'
+
+# i3lock \
+# --insidever-color=$CLEAR     \
+# --ringver-color=$VERIFYING   \
+# \
+# --insidewrong-color=$CLEAR   \
+# --ringwrong-color=$WRONG     \
+# \
+# --inside-color=$VERIFYING        \
+# --ring-color=$DEFAULT        \
+# --line-color=$BLANK          \
+# --separator-color=$DEFAULT   \
+# \
+# --verif-color=$TEXT          \
+# --wrong-color=$TEXT          \
+# --time-color=$TEXT           \
+# --date-color=$TEXT           \
+# --layout-color=$TEXT         \
+# --keyhl-color=$WRONG         \
+# --bshl-color=$WRONG          \
+# \
+# --screen 1                   \
+# --blur 5                     \
+# --clock                      \
+# --indicator                  \
+# --time-str="%H:%M:%S"        \
+# --date-str="%A, %Y-%m-%d"       \
+# --keylayout 1                \
