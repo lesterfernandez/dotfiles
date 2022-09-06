4a7a78321eb7cac01189998b34447e4d9c5850e9
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2022-09-05 20:52:57 -0400

    update some stuffs

--- .config/nvim/lua/me/keymaps.lua
+++ .config/nvim/lua/me/keymaps.lua
@@ -1,6 +1,5 @@
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<leader>,", ",", opts);
-vim.keymap.set("i", "jk", "<ESC>", opts);
 vim.keymap.set("n", "<c-k>", "<c-w><c-k>", opts);
 vim.keymap.set("n", "<c-l>", "<c-w><c-l>", opts);
 vim.keymap.set("n", "<c-h>", "<c-w><c-h>", opts);
@@ -9,4 +8,4 @@ vim.keymap.set("n", "<leader>-", ":resize -3<CR>", opts);
 vim.keymap.set("n", "<leader>+", ":resize +3<CR>", opts);
 vim.keymap.set("n", "<leader>>", ":vertical resize +3<CR>", opts);
 vim.keymap.set("n", "<leader><", ":vertical resize -3<CR>", opts);
-vim.keymap.set("n", "<CR>", ":noh<CR><CR>", opts);
+vim.keymap.set("n", "<CR>", ":noh<CR>", opts);
